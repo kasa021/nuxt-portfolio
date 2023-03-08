ebda319310dafd1f45ef06208f94688086f19e3c
kasa021 <mkafa.k11@gmail.com>
2023-03-08 14:12:00 +0900

    color-modeを使ってdark modeを実装

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,4 +8,15 @@ export default defineNuxtConfig({
       "process.env.DEBUG": false,
     },
   },
+  modules: ["@nuxtjs/color-mode"],
+  colorMode: {
+    preference: "system", // default value of $colorMode.preference
+    fallback: "light", // fallback value if not system preference found
+    hid: "nuxt-color-mode-script",
+    globalName: "__NUXT_COLOR_MODE__",
+    componentName: "ColorScheme",
+    classPrefix: "",
+    classSuffix: "-mode",
+    storageKey: "nuxt-color-mode",
+  },
 });
